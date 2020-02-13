6a6dbe3c18e2e76fdb3de3fdd21b0f27ddbdfaed
Yordanova <gabriela.yordanova@sap.com>
2020-02-13 14:17:24 +0200

    add elasticsearch impl and create console client

--- client/bukmi-cli.py
+++ client/bukmi-cli.py
@@ -1,5 +1,11 @@
 import click
 import requests
+from termcolor import colored
+import json
+
+ADD_API_ENDPOINT = "http://localhost:5000/add"
+SEARCH_API_ENDPOINT = "http://localhost:5000/search"
+LIST_API_ENDPOINT = "http://localhost:5000/list"
 
 @click.group()
 def main():
@@ -8,17 +14,39 @@ def main():
 @main.command('add')
 @click.argument('link')
 def add_bookmark(link):
-    click.echo('Adding bookmark ' + link)
+    body = {'url': link}
+    response = requests.post(ADD_API_ENDPOINT, json = body)
+    click.echo(response.content)
 
 @main.command('search')
-@click.option('--tags', '-t', multiple=True)
-def list_bookmarks(tags):
-    click.echo('Searching bookmarks with tags')
-    print(tags)
+@click.option('--tag', '-t', multiple=True)
+def search_bookmarks(tag):
+    body = {'tags': list(tag)}
+    response = requests.get(SEARCH_API_ENDPOINT, json = body)
+
+    bookmarks = json.loads(response.content)
+    if len(bookmarks) == 0:
+        click.echo("There are no bookmarks matching your request.")
+        return
+        
+    pretty_print(bookmarks)
 
 @main.command('list')
 def list_bookmarks():
-    click.echo('Listing bookmarks')
+    response = requests.get(LIST_API_ENDPOINT)
+
+    bookmarks = json.loads(response.content)
+    if len(bookmarks) == 0:
+        click.echo("There are no bookmarks currently added.")
+        return
+
+    pretty_print(bookmarks)
+
+def pretty_print(bookmarks):
+    index = 1
+    for bookmark in bookmarks:
+        message = "{}. {} \n   > {}".format(index, bookmark['title'], bookmark['url'])
+        click.echo(message)
 
 if __name__ == '__main__':
     main()
